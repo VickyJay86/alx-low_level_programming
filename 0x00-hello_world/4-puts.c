60a63763dfbf2ad385daa3508ef3c51748357696
VickyJay86 <ozorchiemeria@gmail.com>
2023-02-20 06:54:50 -0800

    Task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - print "Programming is like building a multilingual puzzle, followed by a new line.
-* Return: 0.
+*main - print "Programming is like building a multilingual puzzle.
+* Return: Always 0
 */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
