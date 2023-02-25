cbf7bf15905c6d0b7e1158cc100f57f88d02b031
VickyJay86 <ozorchiemeria@gmail.com>
2023-02-25 10:17:33 -0800

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- *
  */
 void print_alphabet_x10(void)
 {
@@ -14,6 +13,5 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z' letter++)
 			_putchar(letter);
 		_putchar('\n');
-
 	}
 }
