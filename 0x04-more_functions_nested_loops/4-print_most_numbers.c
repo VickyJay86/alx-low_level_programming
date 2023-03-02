8c4529d2ce613d9f2f3ca05d1857a639e84b0c59
VickyJay86 <ozorchiemeria@gmail.com>
2023-03-02 03:17:44 -0800

    Task 4-8

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0 - 9, except 2 and 4
+ *
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (!(a == '2' || a == '4'))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @s: size of the square
+ * Return: 0(success)
+ */
+void print_square(int s)
+{
+	if (s <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int a, b;
+
+		for (a = 0; a < s; a++)
+		{
+			for (b = 0; b < s; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+
+			return (0);
+		}
+	}
+}
+
+
