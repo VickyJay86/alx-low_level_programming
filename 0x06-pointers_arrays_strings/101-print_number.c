41a0d4dad65db11fda3d7fc0613a0114072c10a3
VickyJay86 <ozorchiemeria@gmail.com>
2023-03-02 18:32:26 -0800

    Task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,18 +10,16 @@ void print_number(int n)
 {
 	unsigned int x;
 
-	x = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -n;
+		n *= -1;
 	}
 
-	if (x / 10 != 0)
-	{
+	x = n;
+
+	if (x / 10)
 		print_number(x / 10);
-	}
-	_putchar((x % 10) + '0');
-}
 
+	_putchar(x % 10 + '0');
+}
