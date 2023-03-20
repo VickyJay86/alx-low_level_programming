df20ee7a0d9f4ee5cc617c54647333d71c41a538
VickyJay86 <ozorchiemeria@gmail.com>
2023-03-20 08:44:31 -0700

    Initializing struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - store different data types
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
+
