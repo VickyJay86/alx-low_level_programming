19f8ce8c33c4f7b49a16ba9900ccc14e1cc50b21
Chiemeria Ozor <ozorchiemeria@gmail.com>
2023-03-31 04:33:28 -0700

    0x12-singly_linked_lists

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Input char
+ *
+ * Return: 1 (success)
+ * On error, return -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
